bec11619e1d266da95e394ff29dad840506fd664
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2024-04-24 14:09:16 +0900

    update20240424

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import './App.css'
 
 function App() {
     let color = 'green';
-    let name = "Hiroki"
+    let name = "Hiroki";
+    let colors = ['red', 'yellow', 'green', 'blue'];
+    let favoriteNumbers = [7, 77, 43, 90, 81];
     return (
       <>
       <h1>My favorite color is: {color}</h1>
       <h2>hi {name}</h2>
+      <ol>
+        {favoriteNumbers.map(n => {
+          return <li key={n}>{n}</li>
+        })}
+      </ol>
+      <ul>
+        {colors.map((c, index) => {
+          return <li key={index}>{c}</li>
+
+        })}
+      </ul>
       </>
       );
 }
