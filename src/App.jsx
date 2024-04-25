7c30cbcf98968ca360fbaee37e176043a7a4baf3
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2024-04-25 20:31:52 +0900

    update20240425

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,28 @@ function App() {
     let name = "Hiroki";
     let colors = ['red', 'yellow', 'green', 'blue'];
     let favoriteNumbers = [7, 77, 43, 90, 81];
+    function hola() {
+      alert('Hola!!!!!')
+    }
+    function onLeaveFavoriteColor() {
+      console.log('leaving favorite colors')
+    }
     return (
       <>
       <h1>My favorite color is: {color}</h1>
       <h2>hi {name}</h2>
-      <ol>
+      <ol onMouseOver={hola}>
         {favoriteNumbers.map(n => {
           return <li key={n}>{n}</li>
         })}
       </ol>
-      <ul>
+      <ul onMouseLeave={onLeaveFavoriteColor}>
         {colors.map((c, index) => {
           return <li key={index}>{c}</li>
 
         })}
       </ul>
+      <button onClick={hola} >Hola!</button>
       </>
       );
 }
