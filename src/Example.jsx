d38a63096b00cd79cf0aed972cb96c0a9f25b655
hiroki-oso <sonrisa@cap.ocn.ne.jp>
2024-04-26 15:07:45 +0900

    20240426update

---
+++ src/Example.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Example({ color }) {
+  return (
+    <div>
+      <h2>Example: {color}</h2>
+    </div>
+  )
+}
+
+export default Example
---
+++ src/Fun.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Fun({name}) {
+  return (
+    <div>
+       <p>My name is {name}.</p>;
+    </div>
+  )  
+  
+}
+
+export default Fun
